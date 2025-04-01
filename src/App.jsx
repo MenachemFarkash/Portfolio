bfecbe93075a2591e4a82ecc66017828e4d9339a
Menachem Farkash <menachem.farkash1@gmail.com>
2025-04-01 21:23:52 +0300

    Layout and theme overwhole

--- src/App.jsx
+++ src/App.jsx
@@ -6,71 +6,77 @@ import ProjectCard from "./Components/ProjectCard/ProjectCard";
 function App() {
   return (
     <>
-      <header className="main_section-container">
-        <h1>MENACHEM FARKASH</h1>
-        <p>Full Stack Developer & Game Developer</p>
-        <h2>Personal Information</h2>
-        <p>
-          <b>Email:</b> Menachem.farkash1@gmail.com
-        </p>
-        <h3>links:</h3>
-        <div className="contact_links_container">
-          <ContactLink
-            icon="https://static-00.iconduck.com/assets.00/itch-io-icon-2048x2048-6n1nulpw.png"
-            site="itch.io"
-            to="https://p01sen.itch.io/"
-          />
-          <ContactLink
-            icon="https://brandlogos.net/wp-content/uploads/2016/06/linkedin-logo-512x512.png"
-            site="LinkedIn"
-            to="https://www.linkedin.com/in/menachem-farkash-939781256"
-          />
-          <ContactLink
-            icon="https://static-00.iconduck.com/assets.00/github-icon-2048x2048-iw6ut8fe.png"
-            site="GitHub"
-            to="https://github.com/MenachemFarkash?tab=overview&from=2024-12-01&to=2024-12-31"
-          />
-        </div>
-      </header>
-      <section className="education_section_container">
-        <h2>Eduaction</h2>
-        <div className="education_cards_container">
-          <EducationCard
-            school="SVcollege"
-            dates={"2023 - 2024"}
-            details={"Unity Game Developer, design and create 3D and 2D games"}
-            logo="https://svcollege.co.il/wp-content/uploads/2019/04/200_150.png.webp"
-          />
-          <EducationCard
-            school="Cyberpro Israel"
-            dates={"2022 - 2023"}
-            details={"Fullstack Developer, Desing and develop web apps using the MERN stack"}
-            logo="https://media.licdn.com/dms/image/v2/D4E0BAQFR5Cz0DZUueQ/company-logo_200_200/company-logo_200_200/0/1719256427293/cyberpro_israel_logo?e=1749081600&v=beta&t=qx8dvIlKulKqabEvkX-XGMmAh8LWUgEs-5UNYn07l5g"
-          />
-        </div>
-      </section>
-      <section className="projects_cards_container">
-        <h2>Projects</h2>
-        <div className="projects_cards_topics_container">
+      <div className="main_section_container_main">
+        <div className="navbar_container"></div>
+        <section className="education_section_container">
+          <h2>Eduaction</h2>
+          <div className="education_cards_container">
+            <EducationCard
+              school="SVcollege"
+              dates={"2023 - 2024"}
+              details={"Unity Game Developer, design and create 3D and 2D games"}
+              logo="https://svcollege.co.il/wp-content/uploads/2019/04/200_150.png.webp"
+            />
+            <EducationCard
+              school="Cyberpro Israel"
+              dates={"2022 - 2023"}
+              details={"Fullstack Developer, Desing and develop web apps using the MERN stack"}
+              logo="https://media.licdn.com/dms/image/v2/D4E0BAQFR5Cz0DZUueQ/company-logo_200_200/company-logo_200_200/0/1719256427293/cyberpro_israel_logo?e=1749081600&v=beta&t=qx8dvIlKulKqabEvkX-XGMmAh8LWUgEs-5UNYn07l5g"
+            />
+          </div>
+        </section>
+        <section className="main_section-container">
           <div>
-            <h3>Game Development</h3>
-            <div className="projects_cards_topic_container">
-              <ProjectCard />
-              <ProjectCard />
+            <h1>MENACHEM FARKASH</h1>
+            <p>Full Stack Developer & Game Developer</p>
+            <h2>Personal Information</h2>
+            <p>
+              <b>Email:</b> Menachem.farkash1@gmail.com
+            </p>
+          </div>
+          <h3>links:</h3>
+          <div className="contact_links_container">
+            <ContactLink
+              icon="https://static-00.iconduck.com/assets.00/itch-io-icon-2048x2048-6n1nulpw.png"
+              site="itch.io"
+              to="https://p01sen.itch.io/"
+            />
+            <ContactLink
+              icon="https://brandlogos.net/wp-content/uploads/2016/06/linkedin-logo-512x512.png"
+              site="LinkedIn"
+              to="https://www.linkedin.com/in/menachem-farkash-939781256"
+            />
+            <ContactLink
+              icon="https://static-00.iconduck.com/assets.00/github-icon-2048x2048-iw6ut8fe.png"
+              site="GitHub"
+              to="https://github.com/MenachemFarkash?tab=overview&from=2024-12-01&to=2024-12-31"
+            />
+          </div>
+        </section>
+        <section></section>
+        <section className="projects_cards_container">
+          <h2>Projects</h2>
+          <div className="projects_cards_topics_container">
+            <div>
+              <h3>Game Development</h3>
+              <div className="projects_cards_topic_container">
+                <ProjectCard />
+                <ProjectCard />
+              </div>
+            </div>
+            <div>
+              <h3>Web Development</h3>
+              <div className="projects_cards_topic_container">
+                <ProjectCard />
+                <ProjectCard />
+              </div>
             </div>
           </div>
-          <div>
-            <h3>Web Development</h3>
-            <div className="projects_cards_topic_container">
-              <ProjectCard />
-              <ProjectCard />
-            </div>
-          </div>
-        </div>
-      </section>
-      <aside>
-        <h2>Technologies</h2>
-      </aside>
+        </section>
+      </div>
+      <div className="main_section_side_scroll_menu">
+        <aside></aside>
+      </div>
     </>
   );
 }
